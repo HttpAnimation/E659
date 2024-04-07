31d7c47c95e0fd493462e3aa87da8669da7c061e
HttpAnimation <sahwndouglasyt@gmail.com>
2024-04-06 18:08:28 -0700

    adds the runner for rpm in a comment as it only sometimes need it

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
-npm run make -- --platform linux --targets @electron-forge/maker-rpm
+# npm run make -- --platform linux --targets @electron-forge/maker-rpm
 npm run make -- --platform linux
 npm run make -- --platform darwin
